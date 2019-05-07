ddca564f760117e0eee8091b76badfb1b1ef4e0b
mcuthbert <mcuthbert@apple.com>
2019-05-07 13:16:19 -0700

    Containers will restart correctly if docker is restarted

--- api/docker.sh
+++ api/docker.sh
@@ -27,4 +27,5 @@ echo "Starting maproulette api container"
 docker run -t --privileged \
         -d -p 9000:9000 \
         --name maproulette-api $dbLink \
+        -dit --restart unless-stopped \
         maproulette/maproulette-api:${VERSION}
