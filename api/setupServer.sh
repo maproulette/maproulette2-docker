2f4dde24c33584ca33b51ec49f64430a7bd936a3
Lucas Burson <ljdelight@gmail.com>
2021-09-19 17:52:06 -0500

    Refresh the API image docker build

--- api/setupServer.sh
+++ api/setupServer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-PORT=$1
-APIHOST=$2
-if [ -z "$APIHOST" ]; then
-	APIHOST="maproulette.org"
-fi
-if [ -z "$PORT" ]; then
-	PORT=9000
-fi
 
-/MapRouletteAPI/bin/maprouletteapi -Dhttp.port=$PORT -Dconfig.resource=docker.conf \
-	-DAPI_HOST="${APIHOST}" 
+set -exu
+
+APIHOST=${APIHOST:-maproulette.org}
+
+# The java process creates RUNNING_PID and it must not exist before attempting to start java.
+# Delete the file just in case the process died and is restarting.
+rm -f RUNNING_PID
+
+/MapRouletteAPI/bin/maprouletteapi \
+	-Dhttp.port=9000 \
+	-DAPI_HOST="${APIHOST}" \
+	-Dconfig.resource=docker.conf
