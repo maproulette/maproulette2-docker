3bf0241f46f317b0c1aedbb1957252dfccbcc56b
Lucas Burson <ljdelight@gmail.com>
2022-05-29 15:20:19 -0500

    Remove API_HOST variable from swagger
    The deployment scripts were allowing the swagger `Execute` requests to be set to customizable "API host". Since the scala API is what provides the swagger docs there is no need to override the backend API host.
    
    This patch purges the API_HOST variable and the swagger docs will now, by default, send the request to the host that where it's running.
    
    If a different host is needed in the future, use this approach https://github.com/iheartradio/play-swagger#how-do-i-use-a-different-host-for-different-environment

--- api/docker.sh
+++ api/docker.sh
@@ -4,7 +4,6 @@ set -exuo pipefail
 
 export VERSION=$1
 git=(${2//:/ })
-apiHost=$3
 CACHEBUST=${VERSION}
 
 if [ ! -f "api/application-overrides.conf" ]; then
@@ -18,7 +17,7 @@ if [ "$VERSION" = "LATEST" ]; then
     CACHEBUST=$(git ls-remote https://github.com/maproulette/maproulette2.git | grep HEAD | cut -f 1)
 fi
 
-echo "Building container image for MapRoulette API Version: $VERSION, Repo: ${git[1]}, APIHost: $apiHost"
+echo "Building container image for MapRoulette API Version: $VERSION, Repo: ${git[1]}"
 docker build -t maproulette/maproulette-api:"${VERSION}" \
     --build-arg VERSION="${VERSION}" --build-arg GIT="${git[1]}" \
-    --build-arg APIHOST="${apiHost}" --build-arg CACHEBUST="${CACHEBUST}" .
+    --build-arg CACHEBUST="${CACHEBUST}" .
